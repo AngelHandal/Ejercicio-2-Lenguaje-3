b1ea46500468889e970bb9f1862f3c27d4c3c3d0
Angel Handal <97629303+AngelHandal@users.noreply.github.com>
2022-02-18 10:43:30 -0600

    Ejercicio 2 completo

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Ejercicio2_AngelHandal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.registroLst = new System.Windows.Forms.ListBox();
+            this.agregarBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtedad = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(244, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(314, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REGISTROS DE ESTUDIANTES";
+            // 
+            // registroLst
+            // 
+            this.registroLst.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.registroLst.FormattingEnabled = true;
+            this.registroLst.ItemHeight = 19;
+            this.registroLst.Location = new System.Drawing.Point(248, 249);
+            this.registroLst.Name = "registroLst";
+            this.registroLst.Size = new System.Drawing.Size(345, 175);
+            this.registroLst.TabIndex = 1;
+            // 
+            // agregarBtn
+            // 
+            this.agregarBtn.BackColor = System.Drawing.Color.Lime;
+            this.agregarBtn.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.agregarBtn.Location = new System.Drawing.Point(336, 205);
+            this.agregarBtn.Name = "agregarBtn";
+            this.agregarBtn.Size = new System.Drawing.Size(177, 38);
+            this.agregarBtn.TabIndex = 2;
+            this.agregarBtn.Text = "Agregar Registro";
+            this.agregarBtn.UseVisualStyleBackColor = false;
+            this.agregarBtn.Click += new System.EventHandler(this.agregarBtn_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(170, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(195, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Edad";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnombre.Location = new System.Drawing.Point(298, 97);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(151, 22);
+            this.txtnombre.TabIndex = 7;
+            // 
+            // txtedad
+            // 
+            this.txtedad.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtedad.Location = new System.Drawing.Point(298, 145);
+            this.txtedad.Name = "txtedad";
+            this.txtedad.Size = new System.Drawing.Size(151, 22);
+            this.txtedad.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtedad);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.agregarBtn);
+            this.Controls.Add(this.registroLst);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox registroLst;
+        private System.Windows.Forms.Button agregarBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.TextBox txtedad;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio2_AngelHandal{
+    public partial class Form1 : Form{
+        String []nombres = new string[5];  //globales
+        String []edades = new string[5];   //globales
+        public Form1(){
+            InitializeComponent();
+        }
+
+        private void agregarBtn_Click_1(object sender, EventArgs e){
+            //FOR PARA AGREGAR UN NUEVO DATO A NUESTROS ARREGLOS
+            for (int i = 0; i < 1; i++){
+                nombres[i] = txtnombre.Text;
+                edades[i] = txtedad.Text;
+
+            }
+
+            //FOR PARA MOSTRAR NUESTROS ARREGLOS CONCATENADOS,  (EDAD LO TOME COMO STRING PARA PODER CONCATENARLO)
+            for (int i = 0; i < 1; i++){
+                registroLst.Items.Add("Nombre: " + nombres[i] + "   Edad: " + edades[i]);
+
+            }
+        }
+
+    }
+}
